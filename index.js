618ac8f1d5cd3e02605956b51ca972ee2b7bd2be
Runo18 <runo18112000@gmail.com>
2023-09-18 21:01:59 +0700

    array dan object

--- index.js
+++ index.js
@@ -29,3 +29,40 @@ switch (hari) {
     default:
         console.log("Hari ini bukan hari kerja.");
 }
+
+// Membuat array dengan beberapa nilai
+let fruits = ["Apel", "Jeruk", "Pisang", "Anggur"];
+
+// Mengakses elemen-elemen dalam array
+console.log(fruits[0]); // Output: Apel
+console.log(fruits[2]); // Output: Pisang
+
+// Menghitung panjang array
+console.log(fruits.length); // Output: 4
+// Menambah elemen ke dalam array
+fruits.push("Mangga"); // Menambahkan Mangga ke akhir array
+console.log(fruits); // Output: ["Apel", "Jeruk", "Pisang", "Anggur", "Mangga"]
+
+// Menghapus elemen dari array
+fruits.pop(); // Menghapus elemen terakhir (Mangga)
+console.log(fruits); // Output: ["Apel", "Jeruk", "Pisang", "Anggur"]
+
+// Membuat objek dengan beberapa properti
+let person = {
+    nama: "John",
+    usia: 30,
+    pekerjaan: "Insinyur"
+  };
+  
+  // Mengakses properti dalam objek
+  console.log(person.nama); // Output: John
+  console.log(person.usia); // Output: 30
+  
+  // Menambah properti ke dalam objek
+person.alamat = "Jl. Contoh No. 123";
+console.log(person); // Output: { nama: 'John', usia: 30, pekerjaan: 'Insinyur', alamat: 'Jl. Contoh No. 123' }
+
+// Mengubah nilai properti yang sudah ada
+person.usia = 31;
+console.log(person); // Output: { nama: 'John', usia: 31, pekerjaan: 'Insinyur', alamat: 'Jl. Contoh No. 123' }
+
